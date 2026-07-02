f6fa79d9fc404eb89daa1914257f086de0388625
MANISH NISHAD <nishadmanishdinanath@gmail.com>
2026-07-02 23:31:57 +0530

    Complete Python classes 1-4

---
+++ class3.py
@@ -0,0 +1,36 @@
+name = ("manish")
+print(name)
+
+CITY = ("AHMEDAABAD")
+print(CITY)
+
+COLLEGE = ("PARUL UNIVERCITY")
+print(COLLEGE)
+
+surname = ("nishad")
+print(surname)
+
+branch = ("computer science")
+print(branch)
+
+birthplace = ("uttar pradesh")
+print(birthplace)
+
+country = ("india")
+print(country)
+
+school = ("sheth cl hindi high school")
+print(school)   
+
+stream = ("science")
+print(stream)
+
+fathername = ("dinanath nishad")
+print(fathername)
+
+mothername = ("jashodari nishad")
+print(mothername)
+
+hobby = ("workout")
+print(hobby)
+
---
+++ class4.py
@@ -0,0 +1,148 @@
+#1 program to demonstrate the data types in python
+
+
+name = ("manish")
+age = 20 
+height = 5.11
+is_student = True
+
+print(name)
+print(age)
+print(height)
+print(is_student)
+
+
+print(type(name))
+print(type(age))
+print(type(height))
+print(type(is_student))
+
+#2 program to demonstrate the data types in python
+
+movie_name = ("The Matrix")
+rating = 8.5
+tickets_price = 500
+is_hit = True
+
+print(movie_name)
+print(rating)
+print(tickets_price)
+print(is_hit)
+
+print(type(movie_name))
+print(type(rating))
+print(type(tickets_price))
+print(type(is_hit)) 
+
+# 3 program to demonstrate the data types in python
+
+mobile_brand = ("vivo y28")
+ram = 6+6 
+price = 16000
+is_5g = True
+
+print(mobile_brand)
+print(ram)
+print(price)
+print(is_5g)    
+
+print(type(mobile_brand))
+print(type(ram))
+print(type(price))
+print(type(is_5g)) 
+
+# 4 program to demonstrate the data types in python
+
+country = ("india")
+population = 1500000000
+literacy_rate = 74.04
+is_devloped = True
+
+print(country)
+print(population)
+print(literacy_rate)
+print(is_devloped)
+
+print(type(country))
+print(type(population))
+print(type(literacy_rate))
+print(type(is_devloped))
+
+# 5 program to demonstrate the data types in python
+
+state = ("gujarat")
+capital = ("Gandhinagar")
+area = 196024
+is_industrialized = True    
+literacy_rate = 78.03
+
+print(state)   
+print(capital)
+print(area)
+print(is_industrialized)
+print(literacy_rate)
+
+print(type(state))
+print(type(capital))
+print(type(area))
+print(type(is_industrialized))
+print(type(literacy_rate))  
+
+#6 program to demonstrate the data types in python
+
+birthplace = ("uttar pradesh")
+population = 200000000
+literacy_rate = 67.68
+is_devloped = False
+
+print(birthplace)  
+print(population)
+print(literacy_rate)
+print(is_devloped)      
+
+print(type(birthplace))
+print(type(population))
+print(type(literacy_rate))
+print(type(is_devloped))
+
+    #7 program to demonstrate the data types in python
+
+school = ("sheth cl hindi high school")
+class_size = 50
+class_teacher = ("arvind sir")
+is_i_clear_my_exam = True
+
+
+#8 program to demonstrate the data types in python
+
+friend1 = ("sehzan")
+age = 17
+twelve_percent = 35.8
+is_it_pass = True
+
+print(friend1)
+print(age)
+print(twelve_percent)
+print(is_it_pass)   
+
+print(type(friend1))
+print(type(age))
+print(type(twelve_percent))
+print(type(is_it_pass)) 
+
+#9 program to demonstrate the data types in python
+
+sports = ("kabbadi")
+wining_rate = 75.5
+is_it_popular = True
+how_my_tyep_i_won = 10
+
+print(sports)
+print(wining_rate)
+print(is_it_popular)
+print(how_my_tyep_i_won)    
+
+print(type(sports))
+print(type(wining_rate))
+print(type(is_it_popular))
+print(type(how_my_tyep_i_won))  
