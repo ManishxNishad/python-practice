e6085cc701d6dd105cfdbe951dd3f3ae2259eed3
MANISH NISHAD <nishadmanishdinanath@gmail.com>
2026-07-03 08:55:32 +0530

    Class 5 - Input Function Practice

---
+++ class5.py
@@ -0,0 +1,59 @@
+#1st input program 
+
+name = input("enter your name: ")
+city = input("enter your city: ")
+
+
+print("Name:", name)
+print("City:", city)
+
+#2nd input program
+
+name = input("enter your name :")
+college = input("enter your college name :")
+
+print("hello", name)
+print("welcome to", college)
+
+#3rd input program
+
+favorite_movie = input("enter your favorite movie name :")
+favorite_food = input("enter your favorite food name :")
+
+
+print("your favorite movie is :", favorite_movie)
+print("your favorite food is :", favorite_food)
+
+#4th input program 
+
+favorite_sports = input("enter your favorite sport name :")
+father_name = input("enter your father name :")
+
+print("your favorite sport is :", favorite_sports)
+print("your father's name is :", father_name)
+
+
+#5th input program
+
+school_name = input ("your school is : ")
+college_name  =input ("your college is : ")
+favorite_subject = input ("your favorite subject is : ")
+
+print("your school is : ", school_name)
+print("your college is : ", college_name)
+print("your favorite subject is : ", favorite_subject)       
+
+#-------- student information program ---------
+
+name = input("Enter your name ")
+age = input("Enter your age ")
+city  = input ("Enter your city ")
+dream_job = input("Enter your dream job ")
+
+print("---------student information ---------")
+print("name :", name)
+print("age :", age)
+print("city :", city)
+print("dream job :", dream_job)
+
+
