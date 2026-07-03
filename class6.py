e0bf0fa52e775d37a5da629e162d2e6b59c3d2bd
MANISH NISHAD <nishadmanishdinanath@gmail.com>
2026-07-03 12:53:35 +0530

    Class 6 - Type Conversion

---
+++ class6.py
@@ -0,0 +1,82 @@
+#1 Input and Type Conversion 
+
+Age = input("Enter your age : ")
+print(type(Age))
+
+Age = int(Age)
+print(type(Age))
+
+#2 Input and Type Conversion
+
+
+num1 = int(input("enter fisrt number :"))
+num2 = int(input("enter second number :"))
+
+sum = (num1 + num2)
+print("The sum is :", sum)
+
+#3 Input and Type Conversion
+
+height = float(input("enter your height : "))
+print(height)
+print(type(height))
+
+#4 Input and Type Conversion
+
+first_num = int(input("enter first number : "))
+second_num = int(input("enter second number : "))
+sum = first_num + second_num
+print("The sum is :", sum)
+
+#5 Input and Type Conversion
+
+age = int(input("Enter your age : "))
+print(age + 5 ) 
+
+#6 input and Type Conversion
+
+height = float(input("Enter your height : "))
+print(type(height))
+
+#7 input and type convertion
+
+marks = float(input("Enter your marks : "))
+print(marks + 10 )
+
+#8 input and type convertion 
+
+price = int(input("enter the price : "))
+price  = float(price)
+
+print("The price is :", price)
+print(type(price))
+
+#9 input and type convertion    
+
+wieght = (input("Enter your weight : "))
+weight = float(wieght)
+print("The weight is :", weight)
+print(type(weight))
+ 
+ #10 input and type convertion
+
+ salary = int(input("Enter your salary : "))
+ salary = float(salary)
+ print("The salary is :", salary)
+ print(type(salary))    
+
+
+#11 input and type convertion
+
+cgpa = (input("Enter your cgpa : "))
+cgpa = float(cgpa)
+print("The CGPA is :", cgpa)
+print(type(cgpa))
+
+#12 input and type convertion   
+
+user_add_a_number = int(input("Enter a number : "))
+
+print( user_add_a_number + 100)
+
+
