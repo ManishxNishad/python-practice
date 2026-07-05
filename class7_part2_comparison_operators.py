85ae16a2dc0a58bc8c93f8863e1ae526541c9770
MANISH NISHAD <nishadmanishdinanath@gmail.com>
2026-07-05 23:09:21 +0530

    class7_part2_comparison_operators.py

---
+++ class7_part2_comparison_operators.py
@@ -0,0 +1,25 @@
+#practice 1 
+
+print(10 == 10)
+print(10 == 20)
+
+#practice 2
+
+print(10 > 5)
+print(5 > 10)
+
+#practice 3
+
+print(10 != 20)
+print(10 != 10)
+
+#practice 4
+
+print(18 >= 18)
+print(10 <= 20) 
+
+#practice 5
+Age = 18 
+
+print(Age >=  18 )
+
