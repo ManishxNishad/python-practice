f093025fb0508c3536754f4384893f966bbfdfa7
MANISH NISHAD <nishadmanishdinanath@gmail.com>
2026-07-05 23:08:40 +0530

    Class 7 - Part 3 Logical Operators

---
+++ class7_part3_logical_operators.py
@@ -0,0 +1,13 @@
+#practice 1
+
+print(True and True)
+print(True and False)
+print(False and True)
+print(False and False)
+
+#practice 2 
+
+print(True or True)
+print(True or False)
+print(False or True)
+print(False or False)
