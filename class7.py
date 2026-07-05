32aa0d07778dc9a772cad281cd54076f48876968
MANISH NISHAD <nishadmanishdinanath@gmail.com>
2026-07-05 12:15:35 +0530

    Class 7 - Operators Part 1

---
+++ class7.py
@@ -0,0 +1,103 @@
+#program 1 
+
+a = 20 
+b = 10 
+
+print( a + b)
+print ( a - b)
+print ( a * b)
+print ( a / b) 
+
+#program 2
+
+print(10 % 3)
+print(20 % 6 )
+
+#program 3 
+
+print (10//3)
+
+#program 4 
+
+print(5 ** 2)
+print( 3**3 )
+
+#program 5 
+
+x = 10 
+x += 5
+print(x)
+
+#program 6 
+
+x = 50 
+x -= 40 
+print(x)
+
+#practice 1 
+
+num1 = int(input("enter a numer : "))
+num2= int(input("enter a numer : "))
+
+print (num1 + num2 )
+print (num1 - num2 )
+print (num1 * num2 )
+print (num1 / num2 )
+
+
+#practice 2 
+
+num1 = int(input("enter a numer1 : "))
+num2 = int(input("enter a numer2 : "))
+
+print(num1 ** num2)
+
+#practice 3 
+
+num1 = int(input("Enter first number : "))
+num2 = int(input("Enter second number : "))
+
+print(num1 + num2 )
+
+#practice 4
+
+num1 = int(input("Enter first number : "))
+num2 = int(input("Enter second number : "))
+
+print (num1 - num2)
+
+#practice 5 
+
+num1 = int(input("Enter first number : "))
+num2 = int(input("Enter second number : "))
+
+print (num1 * num2 )
+
+#practice 6 
+
+num1 = int(input("Enter first number : "))
+num2 = int(input("Enter second number : "))
+
+print(num1 / num2 )
+
+#practice 7 
+
+num1 = int(input("Enter first number : "))
+num2 = int(input("Enter second number : "))
+
+print(num1 % num2 )
+
+#practice 8
+
+num1 = int(input("Enter first number : "))
+num2 = int(input("Enter second number : "))
+
+print(num1 ** num2 )
+
+#practice 9  
+
+num1 = int(input("Enter first number : "))
+num2 = int(input("Enter second number : "))
+
+print(num1 // num2)
+
