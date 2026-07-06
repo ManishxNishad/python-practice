f511ef3ba3e427f1d2d51aa1a6b11114da6d959c
MANISH NISHAD <nishadmanishdinanath@gmail.com>
2026-07-06 22:25:58 +0530

    Class 8 – Part 1 - If Statement

---
+++ Class 8 – Part 1 - If Statement.py
@@ -0,0 +1,66 @@
+#program 1
+age = 20
+
+if age >=18: 
+    print("Your eligible for vote.")
+
+#program 2
+age = 15
+if age >=18:
+    print("Your eligible for vote.")
+
+#program 3
+marks = 80
+
+if marks >=40:
+    print("Pass.")
+
+
+#program 4
+marks = 25
+
+if marks >=40:
+    print("Pass.")
+
+    #program 5 
+
+age = 20 
+
+if age >=18 : 
+    print("Adult")
+
+#program 6
+
+marks = 100
+
+if marks >=50 :
+    print("Pass.")
+
+#program 7 
+
+number = 10 
+
+if number >= 0 : 
+    print("Positive number ")
+
+
+#program 8 
+
+salary = 50000
+
+if salary >= 30000 : 
+    print("salary is good  ")
+
+#program 9 
+
+age = int(input("Enter your age : "))
+
+if age >= 18 :
+    print("You can vote ")
+
+    #program 10 
+
+marks = int(input("Enter your marks : "))
+
+if marks >= 40 :
+    print("Pass")
