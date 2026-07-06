075de9ef9dc40f9e00e34950aabcd674a4329d8a
MANISH NISHAD <nishadmanishdinanath@gmail.com>
2026-07-06 22:52:02 +0530

    Class 8 - Part 2 Else Statement

---
+++ Class 8 – Part 2 - else Statement.py
@@ -0,0 +1,113 @@
+#program 1
+
+age = 20
+
+if age >= 18:
+
+    print("Adult")
+
+else:
+
+    print("Minor")
+
+#program 2 
+
+age = 15
+
+if age >= 18:
+    print("Adult")
+else:
+    print("Minor")
+
+#program 3 
+
+marks = 80
+
+if marks >= 40:
+    print("Pass")
+else:
+    print("Fail")
+
+#program 4
+
+marks = 25
+
+if marks >= 40:
+    print("Pass")
+else:
+    print("Fail")
+
+#program 5 
+
+number = -5 
+
+if number >=0 :
+    print("Positive number ")
+else: 
+    print("Nevative number ")
+
+#program 6 
+
+age = 22
+
+if age >= 18:
+    print("Eligible to vote")
+else:
+    print("Not eligible")
+
+#program 7
+
+marks = 90
+
+if marks >= 40:
+    print("Pass")
+else:
+    print("Fail")
+
+#program 8 
+
+salary = 25000
+
+if salary >= 30000:
+    print("Good Salary")
+else:
+    print("Improve Skills")
+
+#program 9 
+
+temperature = 28
+
+if temperature > 30:
+    print("Hot")
+else:
+    print("Normal")
+
+#program 10 
+
+number = -8
+
+if number >= 0:
+    print("Positive")
+else:
+    print("Negative")
+
+#program 11 
+
+age = int(input("Enter your age: "))
+
+if age >= 18:
+    print("You can vote")
+else:
+    print("You cannot vote")
+
+#program 12 
+
+age = int(input("Enter your age: "))
+
+if age >= 18:
+    print("You can vote")
+else:
+    print("You cannot vote")
+
+#program 13 
+
